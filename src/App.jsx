9acb76c2cb383178d4914eab3daafda927ce73de
Marline John <marlinejohn@gmail.com>
2023-11-30 17:32:49 +0100

    Added Student component and introduced PropTypes and defaultProps

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Card from "./Card"
 import Button from "./ButtonComponent"
+import Student from "./Student"
 
 function App() {
   return(
@@ -7,6 +8,12 @@ function App() {
       <Card />
       <Card />
       <Card />
+      <Student name ="Marline" age={100} isTrue ={true} />
+      <Student name="ABCD" age={3} isTrue={false}/>
+      <Student name="XYZ" age={40} isTrue={true}/>
+      <Student name="Pizza" age={50} isTrue={false}/>
+      <Student />
+      <Student name="Larry" />
       <div>
       <Button />
       </div>
---
+++ src/Student.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types"
+function Student(props) {
+    return ( 
+        <div className="student">
+            <p>Name:{props.name} </p>
+            <p>Age:{props.age} </p>
+            <p>Student: {props.isTrue ? "Yes": "No"}</p>
+        </div>
+     );
+}
+
+Student.propTypes ={
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.boolean
+}
+
+Student.defaultProps ={
+    name:"Guest",
+    age: 0,
+    isStudent: false
+}
+export default Student;
